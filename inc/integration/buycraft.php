e36cdf994fa77d7250eb12b9a99f8222151cc8b0
Samerton <samerton@users.noreply.github.com>
2015-01-04 20:45:26 +0000

    Create buycraft.php

---
+++ inc/integration/buycraft.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Made by Samerton
+ * http://worldscapemc.co.uk
+ */ 
+if(!isset($queries)){
+	$queries = new Queries();
+}
+$API = $queries->getWhere("settings", array("name", "=", "buycraft_key"))[0]->value;
+ 
+$buycraft = file_get_contents('https://api.buycraft.net/v3?action=payments&secret='.$API);
+
+$buycraft = (json_decode($buycraft, true));
+?>
