9a63a22a9fb02619232bfaacf7af28e8b6600811
Samerton <samerton@users.noreply.github.com>
2015-01-10 13:10:42 +0000

    Create ModernBB converter file

---
+++ converters/modernbb.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+ *	Made by Samerton
+ *  http://worldscapemc.co.uk
+ *
+ *  License: MIT
+ */
+ 
+/*
+ *  Converter from ModernBB
+ *  
+ *  Converts:
+ *     - Bans
+ *     - Categories/Forums
+ *     - Groups
+ *     - Posts and Topics
+ *     - Reports
+ *     - Users
+ */
+
+?>
