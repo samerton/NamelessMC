6f01900e34c3c0ee59f3f61641afb173ca554b88
Samerton <samerton@users.noreply.github.com>
2015-02-01 15:31:47 +0000

    Update init.php
    Update init.php for the new installer

--- inc/init.php
+++ inc/init.php
@@ -1,63 +1,35 @@
 <?php
+
 session_start();
 
-// Uncomment the following 3 lines to enable error reporting
+// Uncomment the following 3 lines to enable error reporting, and comment the 4th and 5th lines
 //ini_set('display_startup_errors',1);
 //ini_set('display_errors',1);
 //error_reporting(-1);
+error_reporting(0);
+ini_set('display_errors', 0);
 
 if(!isset($page)){
 	die();
 }
 
-/* 
- *  Define global variables
+/*
+ *  Autoload classes
  */
 
-$GLOBALS['config'] = array(
-	'mysql' => array(
-		'host' => '', // Web server database IP (Likely to be 127.0.0.1)
-		'username' => '', // Web server database username
-		'password' => '', // Web server database password
-		'db' => '' // Web server database name
-	),
-	'mysql_mc' => array( 
-		'host' => '', // Minecraft database IP
-		'username' => '', // Minecraft database username
-		'password' => '', // Minecraft database password
-		'db' => '' // Minecraft database name
-	),
-	'mysql_bungee' => array( 
-		'host' => '', // Bungee database IP
-		'username' => '', // Bungee database username
-		'password' => '', // Bungee database password
-		'db' => '' // Bungee database name
-	),
-	'remember' => array(
-		'cookie_name' => '', // Name for website cookies
-		'cookie_expiry' => 604800
-	),
-	'session' => array(
-		'session_name' => 'user',
-		'token_name' => 'token'
-	)
-);
+if($path === ""){
+	spl_autoload_register(function($class) {
+		require_once 'inc/classes/' . $class . '.php';
+	});
+	require_once 'inc/functions/sanitize.php';
+} else if($path === "../"){
+	spl_autoload_register(function($class) {
+		require_once '../inc/classes/' . $class . '.php';
+	});
+	require_once '../inc/functions/sanitize.php';
+}
 
-/*
- *  Autoload classes, include sanitize function
- */ 
-
-spl_autoload_register(function($class) {
-	require_once 'inc/classes/' . $class . '.php';
-});
-require_once 'inc/functions/sanitize.php';
-
-/*
- *  Perform page checks
- */
-
-if(strtolower($page) !== "install"){
-
+if($page !== "install"){
 	/*
 	 * Check cookies to see if the user has ticked "remember me" whilst logging in, if so log them in
 	 */
@@ -66,12 +38,16 @@ if(strtolower($page) !== "install"){
 		$hash = Cookie::get(Config::get('remember/cookie_name'));
 		$hashCheck = DB::getInstance()->get('users_session', array('hash', '=', $hash));
 		
-		if ($hashCheck->count()) {
+		if($hashCheck->count()){
 			$user = new User($hashCheck->first()->user_id);
 			$user->login();
 		}
 	}
 
+	/*
+	 *  Initialise users
+	 */
+	
 	if(!isset($user)){
 		$user = new User();
 	}
@@ -89,32 +65,37 @@ if(strtolower($page) !== "install"){
 		}
 	}
 	
+	/*
+	 *  Initialise queries
+	 */
+	
 	if(!isset($queries)){
 		$queries = new Queries();
 	}
 
-}
-
-/* 
- * Install file check 
- */ 
- 
-if(strtolower($page) === "admin"){
-	clearstatcache();
-	if(file_exists("install.php")){
-		Session::flash('adm-alert', '<div class="alert alert-danger">The installation file (install.php) exists. Please remove it!</div>');
-	}
-}
-
-/*
- * Maintenance mode 
- * TODO: Tidy up the message
- */ 
-if(strtolower($page) === "forum"){
-	if($queries->getWhere("settings", array("name", "=", "maintenance"))[0]->value === "true"){
-		if($user->data()->group_id != 2){
-			echo 'Sorry, the forums are in maintenance mode. Please head back to the <a href="../">homepage</a>.';
-			die();
+	/*
+	 * Install file check 
+	 */
+	 
+	if($page === "admin"){
+		clearstatcache();
+		if(file_exists($path . "install.php")){
+			Session::flash('adm-alert', '<div class="alert alert-danger">The installation file (install.php) exists. Please remove it!</div>');
 		}
 	}
+
+	/*
+	 * Maintenance mode 
+	 * TODO: Tidy up the message
+	 */
+	 
+	if($page === "forum"){
+		if($queries->getWhere("settings", array("name", "=", "maintenance"))[0]->value === "true"){
+			if($user->data()->group_id != 2){
+				echo 'Sorry, the forums are in maintenance mode. Please head back to the <a href="../">homepage</a>.';
+				die();
+			}
+		}
+	}
+	
 }
