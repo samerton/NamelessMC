a68e0c7a34c344d8072a194902f1b186dc2d8988
samerton <samerton@users.noreply.github.com>
2015-02-07 14:02:55 +0000

    Update index.php
    Remove password tests

--- pages/index.php
+++ pages/index.php
@@ -6,18 +6,5 @@
  *  License: MIT
  */
 
-require('inc/includes/password.php');
-
-$hash = password_hash('password' . 'asd98023592835uq98aef', PASSWORD_BCRYPT, array("cost" => 13));
-
-$length = 32;
-
-echo strlen($hash) . '<br />';
-
-if(password_verify('password' . 'asd98023592835uq98aef', $hash)){
-	echo 'Valid';
-} else {
-	echo 'Invalid';
-}
 
 ?>
