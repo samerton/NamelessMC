fc8fc5c4a2f11b82b84f3b771b60cecfd70d98dd
Samerton <samerton@users.noreply.github.com>
2015-02-15 12:08:19 +0000

    Update footer.php
    Compatibility with PHP 5.3+

--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -5,22 +5,41 @@
  *
  *  License: MIT
  */
+ 
+$youtube_url = $queries->getWhere("settings", array("name", "=", "youtube_url"));
+$youtube_url = $youtube_url[0]->value;
+
+$twitter_url = $queries->getWhere("settings", array("name", "=", "twitter_url"));
+$twitter_url = $twitter_url[0]->value;
+
+$gplus_url = $queries->getWhere("settings", array("name", "=", "gplus_url"));
+$gplus_url = $gplus_url[0]->value;
+
+$fb_url = $queries->getWhere("settings", array("name", "=", "fb_url"));
+$fb_url = $fb_url[0]->value;
+
+$infractions = $queries->getWhere("settings", array("name", "=", "infractions"));
+$infractions = $infractions[0]->value;
+
+$server_rules_url = ""; // todo
+$forum_rules_url = ""; // todo
+ 
 ?>
       <footer>
-		<?php if($queries->getWhere("settings", array("name", "=", "youtube_url"))[0]->value != "null"){ ?><a href="<?php echo $queries->getWhere("settings", array("name", "=", "youtube_url"))[0]->value; ?>"><i id="social" class="fa fa-youtube-square fa-3x social-gp"></i></a><?php } ?>
-		<?php if($queries->getWhere("settings", array("name", "=", "twitter_url"))[0]->value != "null"){ ?><a href="<?php echo $queries->getWhere("settings", array("name", "=", "twitter_url"))[0]->value; ?>"><i id="social" class="fa fa-twitter-square fa-3x social-tw"></i></a><?php } ?>
-		<?php if($queries->getWhere("settings", array("name", "=", "gplus_url"))[0]->value != "null"){ ?><a href="<?php echo $queries->getWhere("settings", array("name", "=", "gplus_url"))[0]->value; ?>"><i id="social" class="fa fa-google-plus-square fa-3x social-gp"></i></a><?php } ?>
-		<?php if($queries->getWhere("settings", array("name", "=", "fb_url"))[0]->value != "null"){ ?><a href="<?php echo $queries->getWhere("settings", array("name", "=", "fb_url"))[0]->value; ?>"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a><?php } ?>
+		<?php if($youtube_url != "null"){ ?><a href="<?php echo htmlspecialchars($youtube_url); ?>"><i id="social" class="fa fa-youtube-square fa-3x social-gp"></i></a><?php } ?>
+		<?php if($twitter_url != "null"){ ?><a href="<?php echo htmlspecialchars($twitter_url); ?>"><i id="social" class="fa fa-twitter-square fa-3x social-tw"></i></a><?php } ?>
+		<?php if($gplus_url != "null"){ ?><a href="<?php echo htmlspecialchars($gplus_url); ?>"><i id="social" class="fa fa-google-plus-square fa-3x social-gp"></i></a><?php } ?>
+		<?php if($fb_url != "null"){ ?><a href="<?php echo htmlspecialchars($fb_url); ?>"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a><?php } ?>
 		<span class="pull-right">
 
 			<ul class="nav nav-pills dropup">
 				<li class="dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#">&copy; <?php echo htmlspecialchars($queries->getWhere("settings", array("name", "=", "sitename"))[0]->value) . ' ' . date('Y'); ?></a>
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">&copy; <?php echo htmlspecialchars($sitename) . ' ' . date('Y'); ?></a>
 					<ul class="dropdown-menu">
 						<li><a href="/credits">Site software &copy; Samerton</a></li>
 					</ul>
 				</li>
-				<?php if($queries->getWhere("settings", array("name", "=", "infractions"))[0]->value != "false"){ ?>
+				<?php if($infractions != "false"){ ?>
 				<li><a href="/infractions">Infractions</a></li>
 				<?php } ?>
 				<li><a href="/help">Help</a></li>
