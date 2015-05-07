c47658cc075d32c85f07316521d3d6dbb283cc5c
Samerton <samerton@users.noreply.github.com>
2015-05-07 21:19:20 +0100

    Update uuid.php
    Use cURL instead of file_get_contents for the UUID integration class

--- inc/integration/uuid.php
+++ inc/integration/uuid.php
@@ -1,110 +1,144 @@
 <?php
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2014 Daniel Fanara
+ * https://github.com/Shadowwolf97/Minecraft-UUID-Utils
+ *
+ * Modified by Samerton for NamelessMC
+ * https://github.com/samerton/NamelessMC
+ */
+
 class MinecraftProfile {
-	private $username;
-	private $uuid;
-	private $properties;
-	/**
-	* @param string $username The player's username.
-	* @param string $uuid The player's UUID.
-	* @param array $properties The player's properties specified on their Mojang profile.
-	*/
-	function __CONSTRUCT($username, $uuid, $properties = array()) {
-	$this->username = $username;
-	$this->uuid = $uuid;
-	$this->properties = $properties;
-	}
-	/**
-	* @return string The player's username.
-	*/
-	public function getUsername() {
-	return $this->username;
-	}
-	/**
-	* @return string The player's UUID.
-	*/
-	public function getUUID() {
-	return $this->uuid;
-	}
-	/**
-	* @return array The player's properties listed on their mojang profile.
-	*/
-	public function getProperties() {
-	return $this->properties;
-	}
-	/**
-	* @return array Returns an array with keys of 'properties, usernname and uuid'.
-	*/
-	public function getProfileAsArray() {
-	return array("username" => $this->username, "uuid" => $this->uuid, "properties" => $this->properties);
-	}
-	}
-	class ProfileUtils {
-	/**
-	* @param string $identifier Either the player's Username or UUID.
-	* @param int $timeout The length in seconds of the http request timeout.
-	* @return MinecraftProfile|null Returns null if fetching of profile failed. Else returns completed user profile.
-	*/
-	public static function getProfile($identifier, $timeout = 5) {
-	if(strlen($identifier) <= 16){
-	$identifier = ProfileUtils::getUUIDFromUsername($identifier, $timeout);
-	$identifier = $identifier['uuid'];
-	}
-	$url = "https://sessionserver.mojang.com/session/minecraft/profile/".$identifier;
-	$ctx = stream_context_create(array(
-	'http' => array(
-	'timeout' => $timeout
-	)
-	)
-	);
-	$ret = file_get_contents($url, 0, $ctx);
-	if(isset($ret) && $ret != null && $ret != false) {
-	$data = json_decode($ret, true);
-	return new MinecraftProfile($data['name'], $data['id'], $data['properties']);
-	}else {
-	return null;
-	}
-	}
-	/**
-	* @param int $timeout http timeout in seconds
-	* @param $username string Minecraft username.
-	* @return array (Key => Value) "username" => Minecraft username (properly capitalized) "uuid" => Minecraft UUID
-	*/
-	public static function getUUIDFromUsername($username, $timeout = 5) {
-	if(strlen($username) > 16)
-	return array("username" => "", "uuid" => "");
-	$url = 'https://api.mojang.com/profiles/page/1';
-	$options = array(
-	'http' => array(
-	'header' => "Content-type: application/json\r\n",
-	'method' => 'POST',
-	'content' => '{"name":"'.$username.'","agent":"minecraft"}',
-	'timeout' => $timeout
-	),
-	);
-	$context = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
-	// Verification
-	if(isset($result) && $result != null && $result != false)
-	{
-	$ress = json_decode($result, true);
-	$ress = $ress["profiles"][0];
-	$res = Array("username" => $ress['name'], "uuid" => $ress['id']);
-	return $res;
-	}
-	else
-	return null;
-	}
-	/**
-	* @param $uuid string UUID to format
-	* @return string Properly formatted UUID (According to UUID v4 Standards xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx WHERE y = 8,9,A,or B and x = random digits.)
-	*/
-	public static function formatUUID($uuid) {
-	$uid = "";
-	$uid .= substr($uuid, 0, 8)."-";
-	$uid .= substr($uuid, 8, 4)."-";
-	$uid .= substr($uuid, 12, 4)."-";
-	$uid .= substr($uuid, 16, 4)."-";
-	$uid .= substr($uuid, 20);
-	return $uid;
-	}
+    private $username;
+    private $uuid;
+    private $properties;
+
+    /**
+     * @param string $username The player's username.
+     * @param string $uuid The player's UUID.
+     * @param array $properties The player's properties specified on their Mojang profile.
+     */
+    function __CONSTRUCT($username, $uuid, $properties = array()) {
+        $this->username = $username;
+        $this->uuid = $uuid;
+        $this->properties = $properties;
+    }
+
+    /**
+     * @return string The player's username.
+     */
+    public function getUsername() {
+        return $this->username;
+    }
+
+    /**
+     * @return string The player's UUID.
+     */
+    public function getUUID() {
+        return $this->uuid;
+    }
+
+    /**
+     * @return array The player's properties listed on their mojang profile.
+     */
+    public function getProperties() {
+        return $this->properties;
+    }
+
+    /**
+     * @return array Returns an array with keys of 'properties, usernname and uuid'.
+     */
+    public function getProfileAsArray() {
+        return array("username" => $this->username, "uuid" => $this->uuid, "properties" => $this->properties);
+    }
+}
+
+class ProfileUtils {
+    /**
+     * @param string $identifier Either the player's Username or UUID.
+     * @param int $timeout The length in seconds of the http request timeout.
+     * @return MinecraftProfile|null Returns null if fetching of profile failed. Else returns completed user profile.
+     */
+    public static function getProfile($identifier, $timeout = 5) {
+        if(strlen($identifier) <= 16){
+            $identifier = ProfileUtils::getUUIDFromUsername($identifier, $timeout);
+            $url = "https://sessionserver.mojang.com/session/minecraft/profile/".$identifier['uuid'];
+        } else {
+            $url = "https://sessionserver.mojang.com/session/minecraft/profile/".$identifier;
+        }
+		
+		// Use cURL instead of file_get_contents
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		
+		// Execute
+		$ret = curl_exec($ch);
+		
+        if(!empty($ret) && $ret != null && $ret != false) {
+            $data = json_decode($ret, true);
+            return new MinecraftProfile($data['name'], $data['id'], $data['properties']);
+        }else {
+            return null;
+        }
+    }
+
+    /**
+     * @param int $timeout http timeout in seconds
+     * @param $username string Minecraft username.
+     * @return array (Key => Value) "username" => Minecraft username (properly capitalized) "uuid" => Minecraft UUID
+     */
+    public static function getUUIDFromUsername($username, $timeout = 5) {
+        if(strlen($username) > 16)
+            return array("username" => "", "uuid" => "");
+        $url = 'https://api.mojang.com/profiles/page/1';
+		
+		$post_data = '{"name":"'.$username.'","agent":"minecraft"}';
+		
+		// Use cURL instead of file_get_contents
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json',
+			'Content-Length: ' . strlen($post_data))
+		);
+		
+		// Execute
+		$result = curl_exec($ch);
+
+        // Verification
+        if(isset($result) && $result != null && $result != false)
+        {
+            $ress = json_decode($result, true);
+            $ress = $ress["profiles"][0];
+            $res = Array("username" =>  $ress['name'], "uuid" => $ress['id']);
+            return $res;
+        }
+        else
+            return null;
+    }
+
+    /**
+    * @param $uuid string UUID to format
+    * @return string Properly formatted UUID (According to UUID v4 Standards xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx WHERE y = 8,9,A,or B and x = random digits.)
+    */
+    public static function formatUUID($uuid) {
+        $uid = "";
+        $uid .= substr($uuid, 0, 8)."-";
+        $uid .= substr($uuid, 8, 4)."-";
+        $uid .= substr($uuid, 12, 4)."-";
+        $uid .= substr($uuid, 16, 4)."-";
+        $uid .= substr($uuid, 20);
+        return $uid;
+    }
 }
