5a19934b44c75e7ae862c808b1e6d9a40476c0b5
samerton <samerton@users.noreply.github.com>
2015-08-06 23:59:35 +0100

    Fix friends heads moving after hovering on profile
    Fix issue whereby friends avatars would move after hovering over them in profile pages

--- pages/profile.php
+++ pages/profile.php
@@ -283,7 +283,7 @@ if($query_to_use == 'false'){
 					$friends = $user->listFriends($profile_user[0]->id);
 					if($friends !== false){
 						foreach($friends as $friend){
-							echo '<span rel="tooltip" title="' . $user->IdToName($friend->friend_id) . '"><a href="/profile/' . $user->IdToMCName($friend->friend_id) . '"><img class="img-rounded" src="https://cravatar.eu/avatar/' . $user->IdToMCName($friend->friend_id) . '/40.png" /></a></span> ';
+							echo '<span rel="tooltip" title="' . $user->IdToName($friend->friend_id) . '"><a href="/profile/' . $user->IdToMCName($friend->friend_id) . '"><img class="img-rounded" src="https://cravatar.eu/avatar/' . $user->IdToMCName($friend->friend_id) . '/40.png" /></a></span>&nbsp;';
 						}
 					} else {
 						echo 'This user has not added any friends';
